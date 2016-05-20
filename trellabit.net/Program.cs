b2fad1107c172cd52e1e3d1db15145acd6acfacb
DC23 <jugglindan@gmail.com>
2016-05-20 15:53:44 +0800

    added NLog logging support, with basic configuration for console and datestamped file logs

--- trellabit.net/Program.cs
+++ trellabit.net/Program.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
+using NLog;
+using System.IO;
 
 namespace trellabit.net
 {
     class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
-            Console.WriteLine("{0} {1}",
+            logger.Info("{0} {1} online",
+                Assembly.GetExecutingAssembly().GetName().Name,
+                Assembly.GetExecutingAssembly().GetName().Version);
+
+
+
+            logger.Info("{0} {1} offline",
                 Assembly.GetExecutingAssembly().GetName().Name,
                 Assembly.GetExecutingAssembly().GetName().Version);
         }
