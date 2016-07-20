1dda24ed0500c6e0082f13ecba0028938c698b8a
DC23 <jugglindan@gmail.com>
2016-07-20 19:27:59 +0800

    adding testing project

---
+++ trellabit.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("trellabit.tests")]
+[assembly: AssemblyDescription("Unit testing assembly for trellabit.net, a .Net integration between Trello and Habitica.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JugglinDan")]
+[assembly: AssemblyProduct("trellabit.tests")]
+[assembly: AssemblyCopyright("Copyright © JugglinDan 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("42e5b3d7-7b04-431b-8555-cce2f59f7aba")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
