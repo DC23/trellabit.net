4c747a352fdb14cabc214dc81d0ba6ea5a475331
DC23 <jugglindan@gmail.com>
2016-07-25 20:21:51 +0800

    started adding habitica user client

---
+++ data.habitica/clients/HabiticaUserClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trellabit.data.habitica.clients
+{
+    /// <summary>
+    /// The Habitica user client.
+    /// Provides access to Habitica User data and functions.
+    /// </summary>
+    public class HabiticaUserClient
+    {
+    }
+}
