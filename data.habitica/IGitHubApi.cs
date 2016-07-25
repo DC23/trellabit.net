1078b4a4abe7508f0df759e9acfdd3597a254167
DC23 <jugglindan@gmail.com>
2016-07-25 17:15:32 +0800

    adding simple test case for RestEase, which I will remove once the Habitica API is added.

---
+++ data.habitica/IGitHubApi.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using RestEase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trellabit.data.habitica
+{
+    public class User
+    {
+        [JsonProperty("login")]
+        public string Login { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+
+    [Header("User-Agent", "RestEase")]
+    public interface IGitHubApi
+    {
+        [Get("/users/{userId}")]
+        Task<User> GetUserAsync([Path] string userId);
+    }
+
+    public static class GitHubService 
+    {
+        public static User GetUser(string userId)
+        {
+            var api = RestClient.For<IGitHubApi>("https://api.github.com");
+            return api.GetUserAsync(userId).Result;
+        }
+    }
+
+}
---
+++ tests/data.habitica/TestApi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using trellabit.data.habitica;
+using Xunit;
+
+namespace trellabit.tests.data.habitica
+{
+    public class TestApi
+    {
+        [Fact]
+        public void TestGitHubApi()
+        {
+            var user = GitHubService.GetUser("DC23");
+        }
+    }
+}
