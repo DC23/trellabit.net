ad004a1efe2defd4128000bc505a2c0695102f85
DC23 <jugglindan@gmail.com>
2016-07-26 14:42:08 +0800

    adding converter code from habitrpg.dotnet.api

---
+++ services.habitica/converters/TaskConverter.cs
@@ -0,0 +1,72 @@
+//------------------------------------------------------------------------------
+// Original version of this file is from the habitrpg-api-dotnet-client project:
+// https://github.com/marska/habitrpg-api-dotnet-client
+// The original form of these files can be viewed in GitHub commit:
+// https://github.com/DC23/trellabit.net/commit/2f2a304285c347cdb15f1addd70fec7ea3631e57
+// All modifications since that commit are my own.
+//
+// In short, my goals for wrapping the Habitica (HabitRPG) API are quite different.
+// At least initially, I am not trying to wrap the complete API, and I am using
+// a different approach to mechanics of wrapping the API. Finally I am working
+// with Habitica API v3 while habitrpg-api-dotnet-client has been written for
+// API v2. Thus I am not motivated to update the entire habitrpg-api-dotnet-client
+// code to APIv3. However, the model classes are mostly exactly what I need.
+// So rather than write a set of nearly identical JSON.Net annoted serialisation
+// classes, I am reusing these here. Both projects are Apache v2 licensed, so
+// by distributing this code with the same license, along with this header giving
+// acknowledgement of the original source, and the indication of the modifications
+// I have made (via GitHub commits) I believe I am in compliance with the terms
+// of the Apache V2 license.
+//------------------------------------------------------------------------------
+
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace HabitRPG.Client.Converters
+{
+   internal class TaskConverter : CustomCreationConverter<Model.ITask>
+   {
+      public override Model.ITask Create(Type objectType)
+      {
+         throw new NotImplementedException();
+      }
+
+      public Model.ITask Create(Type objectType, JObject jObject)
+      {
+         var type = (string)jObject.Property("type");
+
+         switch (type)
+         {
+            case "daily":
+               return new Model.Daily();
+
+            case "habit":
+               return new Model.Habit();
+
+            case "todo":
+               return new Model.Todo();
+
+            case "reward":
+               return new Model.Reward();
+         }
+
+         throw new Exception(String.Format("Type: {0} not supported", type));
+      }
+
+      public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+      {
+         // Load JObject from stream
+         JObject jObject = JObject.Load(reader);
+
+         // Create target object based on JObject
+         var target = Create(objectType, jObject);
+
+         // Populate the object properties
+         serializer.Populate(jObject.CreateReader(), target);
+
+         return target;
+      }
+   }
+}
---
+++ services.habitica/converters/TimestampConverter.cs
@@ -0,0 +1,67 @@
+//------------------------------------------------------------------------------
+// Original version of this file is from the habitrpg-api-dotnet-client project:
+// https://github.com/marska/habitrpg-api-dotnet-client
+// The original form of these files can be viewed in GitHub commit:
+// https://github.com/DC23/trellabit.net/commit/2f2a304285c347cdb15f1addd70fec7ea3631e57
+// All modifications since that commit are my own.
+//
+// In short, my goals for wrapping the Habitica (HabitRPG) API are quite different.
+// At least initially, I am not trying to wrap the complete API, and I am using
+// a different approach to mechanics of wrapping the API. Finally I am working
+// with Habitica API v3 while habitrpg-api-dotnet-client has been written for
+// API v2. Thus I am not motivated to update the entire habitrpg-api-dotnet-client
+// code to APIv3. However, the model classes are mostly exactly what I need.
+// So rather than write a set of nearly identical JSON.Net annoted serialisation
+// classes, I am reusing these here. Both projects are Apache v2 licensed, so
+// by distributing this code with the same license, along with this header giving
+// acknowledgement of the original source, and the indication of the modifications
+// I have made (via GitHub commits) I believe I am in compliance with the terms
+// of the Apache V2 license.
+//------------------------------------------------------------------------------
+
+﻿using Newtonsoft.Json;
+using System;
+
+namespace HabitRPG.Client.Converters
+{
+   public class TimestampConverter : JsonConverter
+   {
+      private readonly DateTime _epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+      public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+      {
+         var dateTime = ((DateTime)value).ToUniversalTime();
+
+         var intMilliseconds = (Int64)((dateTime - _epoch).TotalMilliseconds);
+
+         writer.WriteRawValue(intMilliseconds.ToString());
+      }
+
+      // SO MANY different values inside a DateTime Property...
+      public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+      {
+         if (reader.Value is DateTime)
+            return reader.Value;
+
+         if (reader.Value == null)
+            return _epoch;
+
+         long longValue;
+
+         if (long.TryParse(reader.Value.ToString(), out longValue))
+            return _epoch.AddMilliseconds(longValue);
+
+         DateTime dateTime;
+
+         if (DateTime.TryParse(reader.Value.ToString(), out dateTime))
+            return dateTime;
+
+         return _epoch;
+      }
+
+      public override bool CanConvert(Type objectType)
+      {
+         return objectType == typeof(DateTime);
+      }
+   }
+}
