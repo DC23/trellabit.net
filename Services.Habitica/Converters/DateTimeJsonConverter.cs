9e69017bcec0e7660be792b216c504e8001c48ac
DC23 <jugglindan@gmail.com>
2016-07-27 15:56:00 +0800

    adding datetime conversion tests

--- Services.Habitica/Converters/DateTimeJsonConverter.cs
+++ Services.Habitica/Converters/DateTimeJsonConverter.cs
@@ -24,44 +24,43 @@ using System;
 
 namespace Trellabit.Services.Habitica.Converters
 {
-   public class DateTimeJsonConverter : JsonConverter
-   {
-      private readonly DateTime _epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+    public class DateTimeJsonConverter : JsonConverter
+    {
+        private readonly DateTime _epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-      public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-      {
-         var dateTime = ((DateTime)value).ToUniversalTime();
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            // TODO: For v3, would it be better to write an ISO8601 string instead of epoch time?
+            var dateTime = ((DateTime)value).ToUniversalTime();
+            var intMilliseconds = (Int64)((dateTime - _epoch).TotalMilliseconds);
+            writer.WriteRawValue(intMilliseconds.ToString());
+        }
 
-         var intMilliseconds = (Int64)((dateTime - _epoch).TotalMilliseconds);
+        // SO MANY different values inside a DateTime Property...
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.Value is DateTime)
+                return reader.Value;
 
-         writer.WriteRawValue(intMilliseconds.ToString());
-      }
+            if (reader.Value == null)
+                return _epoch;
 
-      // SO MANY different values inside a DateTime Property...
-      public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-      {
-         if (reader.Value is DateTime)
-            return reader.Value;
+            long longValue;
 
-         if (reader.Value == null)
+            if (long.TryParse(reader.Value.ToString(), out longValue))
+                return _epoch.AddMilliseconds(longValue);
+
+            DateTime dateTime;
+
+            if (DateTime.TryParse(reader.Value.ToString(), out dateTime))
+                return DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+
             return _epoch;
+        }
 
-         long longValue;
-
-         if (long.TryParse(reader.Value.ToString(), out longValue))
-            return _epoch.AddMilliseconds(longValue);
-
-         DateTime dateTime;
-
-         if (DateTime.TryParse(reader.Value.ToString(), out dateTime))
-            return dateTime;
-
-         return _epoch;
-      }
-
-      public override bool CanConvert(Type objectType)
-      {
-         return objectType == typeof(DateTime);
-      }
-   }
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(DateTime);
+        }
+    }
 }
--- Tests/Services.Habitica/Converters/TestDateTimeJsonConverter.cs
+++ Tests/Services.Habitica/Converters/TestDateTimeJsonConverter.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -19,14 +20,42 @@ namespace Trellabit.Tests.Services.Habitica.Converters
             Assert.True(new DateTimeJsonConverter().CanConvert(typeof(DateTime)));
         }
 
-        [Fact]
+        [Fact(Skip = "fails due to epoch time being written. This test expects a DateTime parseable string")]
         public void WriteJson()
         {
+            // arrange
+            var stringWriter = new StringWriter();
+            var writer = new JsonTextWriter(stringWriter);
+            var serializer = new JsonSerializer();
+            var converter = new DateTimeJsonConverter();
+            var expected = DateTime.Now;
+
+            // act
+            converter.WriteJson(writer, expected, serializer);
+
+            // assert
+            Assert.Equal(expected, DateTime.Parse(stringWriter.ToString()));
         }
 
         [Fact]
-        public void ReadJson()
+        public void ReadJsonUTC()
         {
+            var converter = new DateTimeJsonConverter();
+            var serializer = new JsonSerializer();
+            var reader = new Mock<JsonReader>();
+
+            // OK, we need to get the local time
+            var expected = DateTime.Now;
+            // But make the JsonConverter see it in the equivalent UTC format
+            var expectedUtc = expected.ToUniversalTime();
+            reader.Setup(foo => foo.Value.ToString()).Returns(
+                expectedUtc.ToString("o", CultureInfo.InvariantCulture));
+
+            DateTime actual = (DateTime)converter.ReadJson(reader.Object, typeof(DateTime), null, serializer);
+
+            Assert.IsType<DateTime>(actual);
+            //Assert.Equal(expectedString, actual.ToString("o", CultureInfo.InvariantCulture));
+            Assert.Equal(expected, actual);
         }
     }
 }
